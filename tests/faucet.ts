acca8698e2cf25837ee558b2708b36d66f1ae3e6
Valentin Madrid <valentin@incility.com>
2023-03-29 17:39:54 +0200

    🍉 - Add create Faucet Test

--- tests/faucet.ts
+++ tests/faucet.ts
@@ -6,11 +6,33 @@ describe("faucet", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
+  const USDC_MINT_ADDRESS = new anchor.web3.PublicKey(
+    "4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU"
+  );
+
+  const faucet = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("mint"), USDC_MINT_ADDRESS.toBuffer()],
+    anchor.workspace.Faucet.programId
+  )[0];
+
+  const faucet_token_account = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("token-seed"), USDC_MINT_ADDRESS.toBuffer()],
+    anchor.workspace.Faucet.programId
+  )[0];
+
   const program = anchor.workspace.Faucet as Program<Faucet>;
 
-  it("Is initialized!", async () => {
+  it("Initialize Faucet", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods
+      .initializeFaucet(new anchor.BN(1000000), new anchor.BN(10000))
+      .accounts({
+        mint: USDC_MINT_ADDRESS,
+        faucet: faucet,
+        faucetAccount: faucet_token_account,
+        signer: anchor.workspace.Faucet.provider.wallet.publicKey,
+      })
+      .rpc();
     console.log("Your transaction signature", tx);
   });
 });
